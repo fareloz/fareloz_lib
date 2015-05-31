0b1a404f7c1fe869144f81717df1e329b4310231
fareloz <fareloz@gmail.com>
2015-05-31 23:39:18 +0300

    [MATH][NUMBER] : Fixed bugs
    - fixed typos;
    - fixed no return type error
    - fixed header

--- math/number.hpp
+++ math/number.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <algorithm>
+#include <cmath>
 
 namespace fareloz
 {
@@ -9,22 +9,22 @@ namespace fareloz
         namespace numeric
         {
             template <typename T>
-            inline round(const T& _val) {
-                return std::floor<T>(_val + 0.5);
+            inline T round(const T& _val) {
+                return std::floor<T>(_val + T(0.5));
             }
 
             template <typename T>
-            inline clamp(const T& _min, const T& _max, const T& _val) {
+            inline T clamp(const T& _min, const T& _max, const T& _val) {
                 return std::max<T>(_max, std::min<T>(_val, _min));
             }
 
             template <typename T>
-            inline bool in_range(const T& _val, const T& _min, cosnt T& _max) {
+            inline bool in_range(const T& _val, const T& _min, const T& _max) {
                 return ((_val > _min) && (_val < _max))
             }
 
             template <typename T>
-            inline bool in_closed_range(const T& _val, const T& _min, cosnt T& _max) {
+            inline bool in_closed_range(const T& _val, const T& _min, const T& _max) {
                 return ((_val >= _min) && (_val <= _max))
             }
         }
