e67250f5476c55c87b46ada8f555ebb6bcad1798
fareloz <fareloz@gmail.com>
2015-05-31 23:02:04 +0300

    Use std::floor in round instead of static_cast

--- math/number.hpp
+++ math/number.hpp
@@ -6,14 +6,17 @@ namespace fareloz
 {
     namespace math
     {
-        template <typename T>
-        inline round(const T& _val) { 
-            return static_cast<T>(_val + 0.5);
-        }
+        namespace numeric
+        {
+            template <typename T>
+            inline round(const T& _val) {
+                return std::floor<T>(_val + 0.5);
+            }
 
-        template <typename T>
-        inline clamp(const T& _min, const T& _max, const T& _val) {
-            return std::max<T>(_max, std::min<T>(_val, _min));
+            template <typename T>
+            inline clamp(const T& _min, const T& _max, const T& _val) {
+                return std::max<T>(_max, std::min<T>(_val, _min));
+            }
         }
     }
 }
