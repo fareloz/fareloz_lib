b5af18ac9bdaf1ae22276d21b5deaa001f3a1571
fareloz <fareloz@gmail.com>
2015-08-08 16:04:51 +0300

    [DIAGNOSTIC][PROFILING] : object_counter - Added default functions

--- diagnostic/object_counter.hpp
+++ diagnostic/object_counter.hpp
@@ -18,12 +18,21 @@ namespace fareloz
                 }
 
                 template<typename T>
-                object_counter(const ObjectCounter<T>&) {
+                object_counter(const object_counter<T>&) {
                     ++count;
                 }
 
                 ~object_counter() {
                     --count;
+                }
+
+                template<typename T>
+                object_counter(object_counter<T>&&) = default;
+
+                object_counter& operator=(const object_counter<T>&) = default;
+
+                bool operator==(const object_counter<T>&) {
+                    return true;
                 }
 
                 static inline size_t get_count() {
