81aac6f3088a1f1556d0c0912bd125ea91162377
fareloz <fareloz@gmail.com>
2015-08-10 22:57:32 +0300

    [MATH][COLLECTIONS] : Do range more stl-like
    - Added corresponding typedefs
    - Replaced explicit types with typedefs

--- math/range.hpp
+++ math/range.hpp
@@ -15,29 +15,38 @@ namespace fareloz
                 static_assert(std::is_pod<T>::value, "Template type should be a pod-type");
 
             public:
-                range(T _min, T _max, T _step)
+                typedef T          value_type;
+                typedef T*         pointer;
+                typedef const T*   const_pointer;
+                typedef T&         reference;
+                typedef const T&   const_reference;
+                typedef size_t     size_type;
+                typedef ptrdiff_t  difference_type;
+                typedef range<value_type>  self_type;
+
+                range(value_type _min, value_type _max, value_type _step = value_type(1))
                     : m_min(_min), m_max(_max), m_step(_step) {
                     if (m_step == 0) {
                         throw std::invalid_argument("Step equals zero");
                     }
                 }
 
-                range(const range<T>&) = default;
-                range(range<T>&&) = default;
-                range& operator=(const range<T>&) = default;
+                range(const self_type&) = default;
+                range(self_type&&) = default;
+                range& operator=(const self_type&) = default;
                 ~range() = default;
 
-                bool operator==(const range<T>& _obj) const {
+                bool operator==(const self_type& _obj) const {
                     return (m_max == _obj.max()) &&
                         (m_min == _obj.min()) &&
                         (m_step == _obj.step());
                 }
 
-                bool operator!=(const range<T>& _obj) const {
+                bool operator!=(const self_type& _obj) const {
                     return !(this == _obj);
                 }
 
-                T operator[](size_t _index) const {
+                value_type operator[](size_type _index) const {
 #ifdef _DEBUG
                     if (_index > size()) {
                         throw std::out_of_range("Index out-of-range");
@@ -52,30 +61,30 @@ namespace fareloz
                     return is_empty;
                 }
 
-                size_t size() const {
+                size_type size() const {
                     if (empty()) {
                         return 0;
                     }
                     return ((m_max - m_min) / m_step + 1);
                 }
 
-                T min() const { 
+                value_type min() const {
                     return m_min; 
                 }
 
-                T max() const {
+                value_type max() const {
                     return m_max;
                 }
 
-                T step() const {
+                value_type step() const {
                     return m_step;
                 }
 
             private:
 
-                T m_min;
-                T m_max;
-                T m_step;
+                value_type m_min;
+                value_type m_max;
+                value_type m_step;
             };
         }
     }
