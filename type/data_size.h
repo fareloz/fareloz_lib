8b45062f4bd52ed1cb50a55a6b4e859f021f8437
fareloz <fareloz@gmail.com>
2015-08-18 14:30:18 +0300

    [TYPE] : Added sizes of computer data - mg, kb, etc

---
+++ type/data_size.h
@@ -0,0 +1,15 @@
+
+namespace fareloz
+{
+  namespace type
+  {
+    namespace data_size
+    {
+      static const unsigned long BYTE     = 1;
+      static const unsigned long KILOBYTE = 1024;
+      static const unsigned long MEGABYTE = 1024 * 1024;
+      static const unsigned long GIGABYTE = 1024 * 1024 * 1024;
+      static const unsigned long long TERABYTE = 1024ULL * 1024 * 1024 *1024;
+    };
+  }
+}
