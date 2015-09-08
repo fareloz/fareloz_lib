0c69c170ee2b8a38b1789ac5203675d920b528dd
fareloz <fareloz@gmail.com>
2015-09-08 12:25:13 +0300

    [TYPE][TRAITS] : Rewrite custom traits and add new ones
    - Added is_any - checks if type is_same to any other type (variadic);
    - Added is_bool to check if type is the bool type (no std found);
    - Added is_char to check if type represents one of character types
    (neither signed or unsigned);
    - renamed type-specifier structs;
    - numeric_type rewritten to use more of std traits. Made more general
    and less code

--- type/traits.hpp
+++ type/traits.hpp
@@ -1,37 +1,56 @@
 #pragma once
 
+#include <type_traits>
+
 namespace fareloz
 {
     namespace type
     {
         namespace traits
         {
-            struct UIntType;
-            struct IntType;
-            struct RealType;
-            struct VoidType;
+            template<typename T, typename... Types>
+            struct is_any : std::false_type {};
+
+            template<typename T, typename First>
+            struct is_any<T, First> : std::is_same<T, First> {};
+
+            template<typename T, typename First, typename... Types>
+            struct is_any<T, First, Types...>
+                : std::integral_constant<bool, std::is_same<T, First>::value || is_any<T, Types...>::value>
+            {};
 
             template<typename T>
+            struct is_bool 
+                : std::integral_constant<bool, std::is_same<T, bool>::value>
+            {};
+
+            template<typename T>
+            struct is_char 
+                : std::integral_constant<bool, is_any<T, char, char16_t, char32_t, wchar_t, signed char, unsigned char>::value>
+            {};
+
+            struct void_type;
+            struct float_type;
+            struct int_type;
+            struct uint_type;
+            struct char_type;
+
+            template< typename T,
+                bool is_float = std::is_floating_point<T>::value,
+                bool is_signed = std::is_signed<T>::value,
+                bool is_integral = std::is_integral<T>::value,
+                bool is_char = is_char<T>::value,
+                bool is_bool = is_bool<T>::value >
             struct numeric_type
             {
-                typedef VoidType type;
+                typedef void_type type;
             };
 
-            template<> struct numeric_type<unsigned char>       { typedef UIntType type; };
-            template<> struct numeric_type<unsigned short>      { typedef UIntType type; };
-            template<> struct numeric_type<unsigned int>        { typedef UIntType type; };
-            template<> struct numeric_type<unsigned long>       { typedef UIntType type; };
-            template<> struct numeric_type<unsigned long long>  { typedef UIntType type; };
-
-            template<> struct numeric_type<char>       { typedef IntType type; };
-            template<> struct numeric_type<short>      { typedef IntType type; };
-            template<> struct numeric_type<int>        { typedef IntType type; };
-            template<> struct numeric_type<long>       { typedef IntType type; };
-            template<> struct numeric_type<long long>  { typedef IntType type; };
-
-            template<> struct numeric_type<float>       { typedef RealType type; };
-            template<> struct numeric_type<double>      { typedef RealType type; };
-            template<> struct numeric_type<long double> { typedef RealType type; };
+            template<typename T> struct numeric_type<T, true, true, false, false, false>  { typedef float_type type; };
+            template<typename T> struct numeric_type<T, false, false, true, false, false> { typedef int_type type;   };
+            template<typename T> struct numeric_type<T, false, true, true, false, false>  { typedef uint_type type;  };
+            template<typename T> struct numeric_type<T, false, true, true, true, false>   { typedef char_type type;  };
+            template<typename T> struct numeric_type<T, false, false, true, true, false>  { typedef char_type type;  };
         }
     }
 }
